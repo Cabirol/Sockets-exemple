8053fe2b20e65377c47b5cf8afd5a1bd30bf0c0a
Daniel Acevedo <dlacevedo@protonmail.com>
2022-02-16 00:19:17 +0100

    cors

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
